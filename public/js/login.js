1abd6708452ffc0494224fe8b772a9c135ff24ab
dangminh214 <dangminh214@gmail.com>
2024-03-03 22:36:47 +0100

    after logged in, change direction to homepage: /

--- public/js/login.js
+++ public/js/login.js
@@ -10,12 +10,17 @@ const login = async (email, password) => {
         password
       }
     });
+    //get from json after logged in, src='https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.7/axios.min.js' using this module, this module will send data to user after log in
+    if (res.data.status === 'success') {
+      alert('Logged in successfully');
+      window.setTimeout(()=>{
+        location.assign('/')
+      }, 1500);
+    } 
     console.log(res);
   } catch (err) {
-    console.log(err.response.data);
+    console.log(err.response.data.message);
   }
-  
- 
 };
 document.querySelector('.form').addEventListener('submit', e => { 
   e.preventDefault();
